3c2f10b27eb5d8fe77ee4d3d15c042a82dca0f8e
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-30 04:02:44 +0200

    Create vid2frames.py

---
+++ vid2frames.py
@@ -0,0 +1,35 @@
+from pytubefix import YouTube
+from moviepy.editor import VideoFileClip
+import os
+
+def progress_callback(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage = (bytes_downloaded / total_size) * 100
+    print(f"\rHerunterladen: {percentage:.2f}% abgeschlossen", end='')
+
+def download_youtube_video(url, output_path='video.mp4'):
+    yt = YouTube(url, on_progress_callback=progress_callback)
+    stream = yt.streams.get_highest_resolution()
+    stream.download(filename=output_path)
+    print(f"\nVideo heruntergeladen: {output_path}")
+
+def video_to_frames(video_path, output_folder='frames'):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    try:
+        video = VideoFileClip(video_path)
+        total_frames = int(video.fps * video.duration)
+        for i, frame in enumerate(video.iter_frames(with_times=False, progress_bar=True)):
+            frame_filename = os.path.join(output_folder, f"frame_{i:04d}.jpg")
+            frame.saveframe(frame_filename)
+            print(f"\rUmwandeln: {(i / total_frames) * 100:.2f}% abgeschlossen", end='')
+            print(f"\nExtrahiert {i + 1} Frames nach {output_folder}")
+    except Exception as e:
+        print(f"Fehler beim Verarbeiten des Videos: {e}")
+
+# Beispielaufruf der Funktionen
+video_url = 'https://youtu.be/kXWKaylIhCg?si=TUsuSanc_mVlnKU3'
+download_youtube_video(video_url)
+video_to_frames('video.mp4')
