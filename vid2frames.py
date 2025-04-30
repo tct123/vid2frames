bdb46370c64cef4dd747e448c3fe7a7c14ba4056
tct123 <42028373+tct123@users.noreply.github.com>
2025-04-30 16:44:23 +0200

    Update vid2frames.py

--- vid2frames.py
+++ vid2frames.py
@@ -11,31 +11,34 @@ def progress_callback(stream, chunk, bytes_remaining):
 
 
 def download_youtube_video(url, output_path="video.mp4"):
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    output_path = os.path.join(desktop_path, output_path)
     yt = YouTube(url, on_progress_callback=progress_callback)
     stream = yt.streams.get_highest_resolution()
     stream.download(filename=output_path)
     print(f"\nVideo heruntergeladen: {output_path}")
+    return output_path
 
 
-def video_to_frames(video_path, output_folder="frames"):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+def video_to_frames(video_path):
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop", "frames")
+    if not os.path.exists(desktop_path):
+        os.makedirs(desktop_path)
 
     try:
         video = VideoFileClip(video_path)
         total_frames = int(video.fps * video.duration)
-        for i, frame in enumerate(
-            video.iter_frames(with_times=False, progress_bar=True)
-        ):
-            frame_filename = os.path.join(output_folder, f"frame_{i:04d}.jpg")
+        for i, frame in enumerate(video.iter_frames(with_times=False)):
+            frame_filename = os.path.join(desktop_path, f"frame_{i:04d}.jpg")
             frame.saveframe(frame_filename)
-            print(f"\rUmwandeln: {(i / total_frames) * 100:.2f}% abgeschlossen", end="")
-            print(f"\nExtrahiert {i + 1} Frames nach {output_folder}")
+            progress = (i / total_frames) * 100
+            print(f"\rUmwandeln: {progress:.2f}% abgeschlossen", end="")
+        print(f"\nExtrahiert {i + 1} Frames nach {desktop_path}")
     except Exception as e:
         print(f"Fehler beim Verarbeiten des Videos: {e}")
 
 
 # Beispielaufruf der Funktionen
 video_url = "https://youtu.be/kXWKaylIhCg?si=TUsuSanc_mVlnKU3"
-download_youtube_video(video_url)
-video_to_frames("video.mp4")
+video_path = download_youtube_video(video_url)
+video_to_frames(video_path)
