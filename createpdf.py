50ff965b2364e2bec0f34ecaec6df9d2b57aca0e
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-01 10:38:57 +0200

    Update createpdf.py

--- createpdf.py
+++ createpdf.py
@@ -1,6 +1,7 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import os
+import time
 
 
 def scan_directory(directory_path):
@@ -26,9 +27,11 @@ def create_pdf_with_image(image_folder, pdf_path="output.pdf"):
     # Füge ein Bild hinzu
     try:
         for image in scan_directory(directory_path="frames"):
+            print(f"{image_folder}/{image}")
             c.drawImage(
                 f"{image_folder}/{image}", 100, height - 300, width=200, height=150
             )
+            time.sleep(0.1)
     except:
         pass
 
